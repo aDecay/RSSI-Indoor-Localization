3a88b6ce9d3125184eb795804276aff078cbe845
adecay <younghyeon55@gmail.com>
2024-11-12 17:17:33 +0900

    style: add map setting ui

--- app/src/main/java/com/conviot/rssiindoorlocalization/MapSettingActivity.kt
+++ app/src/main/java/com/conviot/rssiindoorlocalization/MapSettingActivity.kt
@@ -1,34 +1,32 @@
 package com.conviot.rssiindoorlocalization
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.conviot.rssiindoorlocalization.ui.theme.RSSIIndoorLocalizationTheme
 
-class SettingsActivity : ComponentActivity() {
+class MapSettingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             RSSIIndoorLocalizationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Menu(
+                    MapSelector(
                         Modifier.padding(innerPadding)
                     )
                 }
@@ -37,44 +35,29 @@ class SettingsActivity : ComponentActivity() {
     }
 
     @Composable
-    fun Menu(
-        modifier: Modifier = Modifier,
-        menuData: List<MenuData> = listOf(
-            MenuData("지도 설정", MapSettingActivity::class.java),
-            MenuData("키워드 설정", SettingsActivity::class.java)
-        )
-    ) {
+    fun MapSelector(modifier: Modifier = Modifier) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
             modifier = modifier.fillMaxSize()
         ) {
-            for (data in menuData) {
-                MenuIntentButton(data.text, data.cls, Modifier.width(200.dp))
+            Image(
+                painter = painterResource(R.drawable.empty),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+                modifier = modifier.fillMaxWidth()
+            )
+            Button({}) {
+                Text(text = "이미지 선택")
             }
-        }
-    }
-
-
-    @Composable
-    fun MenuIntentButton(text: String, cls: Class<*>, modifier: Modifier) {
-        val context = LocalContext.current
-
-        Button(
-            onClick = {
-                context.startActivity(Intent(context, cls))
-            },
-            modifier = modifier
-        ) {
-            Text(text = text)
         }
     }
 
     @Preview(showBackground = true)
     @Composable
-    fun MenuPreview() {
+    fun MapSelectorPreview() {
         RSSIIndoorLocalizationTheme {
-            Menu()
+            MapSelector()
         }
     }
 }
