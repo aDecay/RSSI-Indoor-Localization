ecf49bfc6d13139c9180a39efa4b87be90247595
adecay <younghyeon55@gmail.com>
2024-11-13 10:10:02 +0900

    feat: select and save map

--- app/src/main/java/com/conviot/rssiindoorlocalization/MapSettingActivity.kt
+++ app/src/main/java/com/conviot/rssiindoorlocalization/MapSettingActivity.kt
@@ -1,9 +1,14 @@
 package com.conviot.rssiindoorlocalization
 
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,12 +18,18 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import coil3.compose.AsyncImage
 import com.conviot.rssiindoorlocalization.ui.theme.RSSIIndoorLocalizationTheme
+import java.io.File
+import java.io.IOException
 
 class MapSettingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,20 +45,54 @@ class MapSettingActivity : ComponentActivity() {
         }
     }
 
+    @Throws(IOException::class)
+    private fun readBytes(context: Context, uri: Uri): ByteArray? =
+        context.contentResolver.openInputStream(uri)?.use { it.buffered().readBytes() }
+
     @Composable
     fun MapSelector(modifier: Modifier = Modifier) {
+        val filename = "map"
+
+        var imageUri by remember {
+            mutableStateOf<Uri?>(null)
+        }
+
+        val file = File(applicationContext.filesDir, filename)
+        if (file.exists()) {
+            imageUri = Uri.fromFile(file)
+        }
+
+        val pickMedia = rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                imageUri = uri
+
+                val fileContents = readBytes(applicationContext, uri)
+                applicationContext.openFileOutput(filename, Context.MODE_PRIVATE).use {
+                    it.write(fileContents)
+                }
+
+                Log.d("PhotoPicker", "Selected URI: $uri")
+            } else {
+                Log.d("PhotoPicker", "No media selected")
+            }
+        }
+
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
             modifier = modifier.fillMaxSize()
         ) {
-            Image(
-                painter = painterResource(R.drawable.empty),
+            AsyncImage(
+                model = imageUri,
                 contentDescription = null,
                 contentScale = ContentScale.FillWidth,
                 modifier = modifier.fillMaxWidth()
             )
-            Button({}) {
+            Button(
+                onClick = {
+                    pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                }
+            ) {
                 Text(text = "이미지 선택")
             }
         }
