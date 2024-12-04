66f96997a5a86c6724d13972b2211af09a84b05a
nadering <120453768+nadering@users.noreply.github.com>
2024-12-04 21:48:03 +0900

    Feat: TestActivity에 사용자 위치 추정 기능 추가
    - TestActivity에 사용자 위치 추정 기능 추가
      - 테스트 버튼 클릭 시, 지정되어 있는 CSV 파일에서 RSSI 값을 받아온 후, 모델에 입력시켜 출력값을 받아와 위치 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.tensorflow.lite)
 }
 
 protobuf {
--- app/src/main/java/com/conviot/rssiindoorlocalization/TestActivity.kt
+++ app/src/main/java/com/conviot/rssiindoorlocalization/TestActivity.kt
@@ -42,6 +42,8 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -63,7 +65,18 @@ import coil3.compose.AsyncImage
 import com.conviot.rssiindoorlocalization.data.UserPreferencesSerializer
 import com.conviot.rssiindoorlocalization.datastore.UserPreferences
 import com.conviot.rssiindoorlocalization.ui.theme.RSSIIndoorLocalizationTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.tensorflow.lite.Interpreter
+import java.io.BufferedReader
 import java.io.File
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.util.Arrays
 import kotlin.random.Random
 
 /** RSSI 데이터를 통한 사용자의 위치 확인 기능을 테스트하는 Activity */
@@ -112,6 +125,110 @@ class TestActivity : ComponentActivity() {
         }
     }
 
+    /** TFLite 모델 로드 후 반환 */
+    suspend fun loadModel(context: Context): Interpreter = withContext(Dispatchers.IO) {
+        // 파일 및 Context
+        val modelPath = "model.tflite"
+        val assetManager = context.assets
+
+        // 모델 파일 읽기
+        val inputStream = assetManager.open(modelPath)
+        val modelBytes = inputStream.readBytes()
+        inputStream.close()
+
+        // ByteArray -> ByteBuffer 변환
+        val byteBuffer = ByteBuffer.allocateDirect(modelBytes.size).apply {
+            order(ByteOrder.nativeOrder()) // 플랫폼에 맞는 ByteOrder 사용
+            put(modelBytes)
+            rewind() // ByteBuffer의 위치를 0으로 초기화
+        }
+
+        // Interpreter 생성
+        Interpreter(byteBuffer)
+    }
+
+    /** CSV 파일에서 학습에 사용된 BSSID 리스트를 반환 */
+    fun loadBssidListFromCsv(context: Context): List<String> {
+        // 파일 및 Context
+        val csvFileName = "bssid.csv" // CSV 파일 이름
+        val assetManager = context.assets
+
+        return assetManager.open(csvFileName).bufferedReader().use { reader ->
+            reader.readLines().flatMap { line ->
+                line.split(",").map { it.trim() } // CSV의 각 셀을 BSSID로 추출
+            }
+        }
+    }
+
+    /** 하나의 record_id만 들어있도록 가공된 CSV 파일에서, WifiInfo 리스트를 반환 */
+    fun parseCsvToWifiInfoList(context: Context): MutableList<DataCollectActivity.WifiInfo> {
+        // 파일 및 Context
+        val csvFileName = "rssi_single_record_550.csv"
+        val assetManager = context.assets
+
+        // record_id 하나의 Wifi 리스트
+        val wifiInfoList = mutableListOf<DataCollectActivity.WifiInfo>()
+
+        // CSV 파일 읽기
+        BufferedReader(
+            InputStreamReader(
+                assetManager.open(csvFileName),
+                Charsets.UTF_8
+            )
+        ).use { reader ->
+            // 첫 번째 라인(헤더) 건너뛰기
+            reader.readLine()
+
+            // 데이터 파싱
+            reader.lineSequence().forEach { line ->
+                val values = line.split(",")
+                if (values.size >= 4) {
+                    val ssid = values[1]
+                    val bssid = values[2]
+                    val rssi = values[3].toIntOrNull() ?: -100
+                    wifiInfoList.add(DataCollectActivity.WifiInfo(ssid, bssid, rssi))
+                }
+            }
+        }
+
+        Log.d(
+            "ParseCsvToWifiInfoList",
+            "Index 0: ${wifiInfoList[0].ssid} / ${wifiInfoList[0].bssid} / ${wifiInfoList[0].rssi}"
+        )
+        return wifiInfoList
+    }
+
+    /** TFLite 모델 실행 */
+    suspend fun runModel(
+        context: Context,
+        interpreter: Interpreter,
+        wifiList: SnapshotStateList<DataCollectActivity.WifiInfo>
+    ): Pair<Float, Float> = withContext(Dispatchers.IO) {
+        // 학습된 BSSID 리스트
+        val trainedBssidList = loadBssidListFromCsv(context)
+        Log.d("RunModel_TrainedBssidList", trainedBssidList.toString())
+
+        val rssiMap = wifiList.associate { it.bssid to it.rssi.toFloat() } // BSSID-RSSI 매핑 생성
+
+        // 학습된 BSSID 순서에 따라 RSSI 값을 정렬 후, 입력값으로 사용
+        val inputArray = trainedBssidList.map { bssid ->
+            val rssi = rssiMap[bssid] ?: -100f  // rssiMap에 값이 없으면 -100f로 설정
+            if (rssi < -85f) -100f else rssi    // 값이 -85f 미만이면 -100f로 설정
+        }.toFloatArray()
+
+        Log.d("RunModel_Input", Arrays.toString(inputArray))
+
+        // 출력 크기 맞추기 (예: [[x, y]])
+        val outputArray = Array(1) { FloatArray(2) }
+
+        // 모델 실행
+        interpreter.run(inputArray, outputArray)
+
+        // 결과 반환
+        Log.d("RunModel_Output", "X: ${outputArray[0][0]}, Y: ${outputArray[0][1]}")
+        Pair(outputArray[0][0], outputArray[0][1])
+    }
+
     /** RSSI 데이터를 수집하고, 컴포넌트를 표시 */
     @Composable
     fun DataCollector(
@@ -142,21 +259,46 @@ class TestActivity : ComponentActivity() {
 
         /** RSSI 데이터를 통해 사용자의 위치를 확인 */
         fun testLocalization(dataCollectViewModel: DataCollectViewModel) {
-            /**
-             * ToDo:
-             * 1. dataCollectViewModel.wifiList에서 현재 위치의 SSID, BSSID, RSSI 값을 획득
-             * 2-1. Feature로 사용된 BSSID가 현재 위치의 BSSID에 있을 경우: 해당 BSSID의 RSSI 값을 설정
-             * 2-2. Feature로 사용된 BSSID가 현재 위치의 BSSID에 없을 경우: 결측값의 RSSI 값을 -200으로 설정
-             * 2-3. 현재 위치의 BSSID에는 있지만 Feature로 사용된 BSSID에 없을 경우: 넘어감
-             * 3. RSSI를 설정한 후, 모델에 해당 값을 보내 사용자의 위치를 계산하고 Localization 관련 변수 설정
-             * 4. 해당 변수들로 지도에 점을 찍어 사용자의 현재 위치를 나타냄
-             */
-            // 임시
-            localizationX.value = Random.nextFloat()
-            localizationY.value = Random.nextFloat()
-            localizationTested.value = true
+            CoroutineScope(Dispatchers.Main).launch {
+                try {
+                    // 모델 로드
+                    val interpreter = loadModel(this@TestActivity)
 
-            Log.d("testLocalization", "X: ${localizationX.value}, Y: ${localizationY.value}")
+                    /*
+                    // 모델 실행 (실제로 측정된 WifiInfo 리스트 사용)
+                    val (x, y) = runModel(
+                        this@TestActivity,
+                        interpreter,
+                        dataCollectViewModel.wifiList
+                    )
+                    */
+
+                    // 모델 실행 (테스트 전용, 하나의 record_id에 대한 데이터 사용)
+                    val _tempWifiList = parseCsvToWifiInfoList(this@TestActivity)
+                    val tempWifiList = _tempWifiList.toMutableStateList()
+
+                    val (x, y) = runModel(
+                        this@TestActivity,
+                        interpreter,
+                        tempWifiList
+                    )
+
+                    // 모델 종료
+                    interpreter.close()
+
+                    // 사용자 위치 설정
+                    localizationX.value = x
+                    localizationY.value = y
+                    localizationTested.value = true
+
+                    Log.d(
+                        "TestLocalization",
+                        "X: ${localizationX.value}, Y: ${localizationY.value}"
+                    )
+                } catch (e: Exception) {
+                    Log.e("TestLocalization", "Error during model execution: ${e.message}")
+                }
+            }
         }
 
         Box(
@@ -209,7 +351,9 @@ class TestActivity : ComponentActivity() {
     ) {
         // 파란 점을 그릴 Canvas
         Canvas(
-            modifier = modifier.fillMaxSize().statusBarsPadding()
+            modifier = modifier
+                .fillMaxSize()
+                .statusBarsPadding()
         ) {
             // 지도 좌표 변환
             val pointX = localizationX * dataCollectViewModel.imageWidth
